074502f2eb1c30463314ebb703b90863a91ad4d2
JwHanson <jw.hanson@outlook.com>
2019-03-26 15:13:55 -0600

    Imported and added my rubegoldberg machine to SampleScene.

---
+++ Assets/HansonRubeGoldberg/Scripts/ballupdate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballupdate : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.up * 0.001f;
+	}
+}
---
+++ Assets/HansonRubeGoldberg/Scripts/startMachine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startMachine : MonoBehaviour
+{
+
+    [SerializeField] private Animator lengthenRope;
+    [SerializeField] private Animator shortenRope;
+    [SerializeField] private Animator tiltPlank;
+    //[SerializeField] private Animation lowerPlatform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Start");
+        lengthenRope.enabled = true;
+        shortenRope.enabled = true;
+        tiltPlank.enabled = true;
+        GetComponent<Animator>().enabled = true;
+    }
+}
